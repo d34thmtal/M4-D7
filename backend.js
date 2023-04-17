0c42c005e77877374c0409611bda0baa1f34881b
d34thmtal <max.cossu@hotmail.com>
2023-04-17 16:48:15 +0200

    m4 d7 funzionante

--- backend.js
+++ backend.js
@@ -1,22 +1,59 @@
 const apiUrl = `https://striveschool-api.herokuapp.com/api/product/`
-const apiKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM2ZjI0YzMzYjE1MjAwMTQ3NjE3OWEiLCJpYXQiOjE2ODEzMjMzOTIsImV4cCI6MTY4MjUzMjk5Mn0.C2DTAofGOHlD43flBnMxKcXHR1Z99cqPLMFyteE7oXA`
+const apiKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM2ZjI0YzMzYjE1MjAwMTQ3NjE3OWEiLCJpYXQiOjE2ODE3MzY4MjIsImV4cCI6MTY4Mjk0NjQyMn0.mUapuxMbdtuyrZx-onFCQmLaZp-vXsqwdMDrb1pOgMY`
 
 const form = document.getElementById("user-form")
-
+const idProductNuovo = document.getElementById('prodotto-id')
 const nameInput = document.getElementById("name")
 const descriptionInput = document.getElementById("description")
 const brandInput = document.getElementById("brand")
 const imageUrlInput = document.getElementById("image-url")
 const priceInput = document.getElementById("price")
 
-// const createdAt = Date.now().getTime() GENERA L'ORA DI CREAZIONE
-// const updatedAt = Date.now().getTime()
+// form.addEventListener('submit', async (event) =>{
+//     event.preventDefault();
+//     const product = {
+//         name: nameInput.value,
+//         description: descriptionInput.value,
+//         brand: brandInput.value,
+//         imageUrl: imageUrlInput.value,
+//         price: priceInput.value
+//     }
 
-
+//     try {
+//         if(idProductNuovo.value === ""){
+//             const risposta = await fetch(`${apiUrl}${idProductNuovo.value}`, {
+//                 method: 'PUT',
+//                 body: JSON.stringify(product),
+//                 headers: new Headers ({
+//                     "Authorization": `Bearer ${apiKey}`,
+//                     'Content-type': 'application/json; charset=UTF-8'
+//                 })
+//             })
+//             if (risposta.ok) {
+//                 window.location.href = `backend.html?status=edit-ok`
+//             } else {
+//                 alert('Errore durante la modifica dell\'prodotto')
+//             }
+//         } else {
+//             const response = await fetch(apiUrl, {
+//                 method : 'POST',
+//                 body : JSON.stringify(product),
+//                 headers : new Headers ({
+//                     "Authorization": `Bearer ${apiKey}`,
+//                     "Content-type" : 'application/json; charset=UTF-8'
+//                 })
+//             })
+//             window.location.href = `backend.html?status=aggiunta-prodotto`
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
 
 form.addEventListener('submit', async (event) =>{
     event.preventDefault();
     const product = {
+        // _id: idProductNuovo.value,
         name: nameInput.value,
         description: descriptionInput.value,
         brand: brandInput.value,
@@ -24,24 +61,8 @@ form.addEventListener('submit', async (event) =>{
         price: priceInput.value
     }
 
-    const idProductNuovo = document.getElementById('id-product')
-
     try {
-        if(idProductNuovo.innerHTML){
-            const response = await fetch(`${apiUrl}${idProductNuovo.innerHTML}`, {
-                method: 'PUT',
-                body: JSON.stringify(product),
-                headers: new Headers ({
-                    "Authorization": `Bearer ${apiKey}`,
-                    'Content-type': 'application/json; charset=UTF-8'
-                })
-            })
-            if (response.ok) {
-                window.location.href = `backend.html?status=edit-ok`
-            } else {
-                alert('Errore durante la modifica dell\'prodotto')
-            }
-        } else {
+        if(idProductNuovo.value === "" ){
             const response = await fetch(apiUrl, {
                 method : 'POST',
                 body : JSON.stringify(product),
@@ -50,8 +71,22 @@ form.addEventListener('submit', async (event) =>{
                     "Content-type" : 'application/json; charset=UTF-8'
                 })
             })
-            window.location.href = `backend.html?status=aggiunta-prodotto`
-        }
+            window.location.href = 'backend.html?status=aggiunta-prodotto'
+        } else  {
+            const risposta = await fetch(`${apiUrl}${idProductNuovo.value}`, {
+                method: 'PUT',
+                body: JSON.stringify(product),
+                headers: new Headers ({
+                    "Authorization": `Bearer ${apiKey}`,
+                    'Content-type': 'application/json; charset=UTF-8'
+                })
+            })
+            if (risposta.ok) {
+                window.location.href = 'backend.html?status=edit-ok'
+            } else {
+                alert('Errore durante la modifica dell\'prodotto')
+            }
+        } 
     } catch (error) {
         console.log(error);
     }
@@ -74,14 +109,13 @@ async function getToken () {
 
 getToken()
 
-//creazione tabella prodotti
 function tableBody(product) {
     const tableProd = document.getElementById('table-body');
     tableProd .innerHTML = '';
     product.forEach((element) => {
         const row = `
         <tr>
-            <td id="id-product" class="td-body">${element._id}</td>
+            <td id="prodotto-id" class="td-body">${element._id}</td>
             <td class="td-body">${element.name}</td>
             <td class="td-body">${element.description}</td>
             <td class="td-body">${element.brand}</td>
@@ -117,7 +151,6 @@ async function deleteProduct(deleteProductId) {
     }
 }
 
-
 function titoloPage (titolo) {
     const titlePage = document.getElementById(`page-title`)
     titlePage.textContent = titolo ? `Modifica Prodotto` : `Crea Prodotto`
@@ -127,16 +160,15 @@ function goBack() {
     window.location.href = 'index.html'
 }
 
-
 async function getProductData(idProdotto) {
-    const idProductNuovo = document.getElementById('id-product')
     try {
         const response = await fetch(`${apiUrl}${idProdotto}`,{
             headers: {
                 "Authorization" : `Bearer ${apiKey}`
         }
     })
-    const product = await response.json() 
+    const product = await response.json()
+        idProductNuovo.value = product._id
         nameInput.value = product.name
         descriptionInput.value = product.description
         brandInput.value = product.brand
@@ -153,3 +185,25 @@ function buildPageTitle(nameInput) {
     pageTitle.textContent = nameInput ? 'Modifica utente' : 'Crea nuovo utente'
 }
 
+//autenticazione 
+// async function rinnovoToken() {
+
+//     try {
+//         const response = await fetch(apiUrl, {
+//             method: 'POST',
+//             endpoint: `https://striveschool-api.herokuapp.com/api/account/login`,
+//             requestbody:
+//             {
+//             "username": `max.cossu@hotmail.com`,
+//             "password": 'Massimo85!'
+//             }
+//         })
+//     }  
+//     catch (error) {
+//         console.log('Errore nel recupero della password ', error);
+//     } 
+// }
+
+// windows.onload = function () {
+    
+// }
